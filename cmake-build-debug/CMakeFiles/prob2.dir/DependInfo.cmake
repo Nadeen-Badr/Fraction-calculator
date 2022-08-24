bd3b6b7a7f164f1194e0c535f5c60967ede0baff
Nadeen-Badr <108485450+Nadeen-Badr@users.noreply.github.com>
2022-08-24 23:54:54 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/prob2.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/FCAI.CU/Second Level/Second Semester/Data structure/ASs/prob2/fraction.cpp" "D:/FCAI.CU/Second Level/Second Semester/Data structure/ASs/prob2/cmake-build-debug/CMakeFiles/prob2.dir/fraction.cpp.obj"
+  "D:/FCAI.CU/Second Level/Second Semester/Data structure/ASs/prob2/fractionCalculator.cpp" "D:/FCAI.CU/Second Level/Second Semester/Data structure/ASs/prob2/cmake-build-debug/CMakeFiles/prob2.dir/fractionCalculator.cpp.obj"
+  "D:/FCAI.CU/Second Level/Second Semester/Data structure/ASs/prob2/prob2.cpp" "D:/FCAI.CU/Second Level/Second Semester/Data structure/ASs/prob2/cmake-build-debug/CMakeFiles/prob2.dir/prob2.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/prob2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prob2.dir/fraction.cpp.obj"
+  "CMakeFiles/prob2.dir/fractionCalculator.cpp.obj"
+  "CMakeFiles/prob2.dir/prob2.cpp.obj"
+  "libprob2.dll.a"
+  "prob2.exe"
+  "prob2.exe.manifest"
+  "prob2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prob2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
